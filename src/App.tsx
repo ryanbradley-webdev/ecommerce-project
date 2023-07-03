751e5a026789d1de19e7d4a83822456237cd3381
Ryan Bradley <ryanbradley315@gmail.com>
2023-07-03 12:44:13 -0500

    Add routes for products and description pages.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { MantineProvider } from "@mantine/core";
 import { Route, Routes } from "react-router-dom";
+import Home from "./routes/home/Home";
+import Products from "./routes/products/Products";
+import ProductDescription from "./routes/products/ProductDescription";
 
 export default function App() {
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
       <Routes>
-        <Route path="/" element={'Home'} />
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:id" element={<ProductDescription />} />
       </Routes>
     </MantineProvider>
   )
