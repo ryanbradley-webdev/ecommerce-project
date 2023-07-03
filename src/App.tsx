01455cebec91e161cf53a84dbdb573df1b64f9ae
Ryan Bradley <ryanbradley315@gmail.com>
2023-07-03 13:01:37 -0500

    Start basic page styling.

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Products from "./routes/products/Products";
 import ProductDescription from "./routes/products/ProductDescription";
 import Header from "../components/header/Header";
 import Footer from "../components/footer/Footer";
+import './App.css'
 
 export default function App() {
   return (
