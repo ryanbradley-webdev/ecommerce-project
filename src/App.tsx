5f71136d47b10e4e12db49d716214f7c169f6f27
Ryan Bradley <ryanbradley315@gmail.com>
2023-07-03 12:46:07 -0500

    Add header and footer components.

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,23 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./routes/home/Home";
 import Products from "./routes/products/Products";
 import ProductDescription from "./routes/products/ProductDescription";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 
 export default function App() {
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
+
+      <Header />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
         <Route path="/products/:id" element={<ProductDescription />} />
       </Routes>
+
+      <Footer />
+
     </MantineProvider>
   )
 }
