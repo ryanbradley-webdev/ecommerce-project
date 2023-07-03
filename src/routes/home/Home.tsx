88c25292307cdaffb9e1ecea392620298785c825
Ryan Bradley <ryanbradley315@gmail.com>
2023-07-03 14:48:37 -0500

    Update hompage cards to be within a carousel.

--- src/routes/home/Home.tsx
+++ src/routes/home/Home.tsx
@@ -1,7 +1,94 @@
+import { Carousel } from "@mantine/carousel";
+import { Button, Card, Stack, MediaQuery, Text } from "@mantine/core";
+
 export default function Home() {
     return (
         <main>
-            Home
+
+            <Stack
+                spacing={24}
+            >
+                
+                <MediaQuery
+                    query='(max-width: 480px)'
+                    styles={{ fontSize: '24px' }}
+                >
+
+                    <h1 style={{ textAlign: 'center' }}>
+                        Wecome to Commerce City
+                    </h1>
+
+                </MediaQuery>
+
+                <Carousel
+                    slideSize='80%'
+                    slideGap='lg'
+                    withIndicators
+                    styles={{
+                        indicator: {
+                            background: 'var(--color-indicator)'
+                        }
+                    }}
+                >
+
+                    <Carousel.Slide>
+                    <Card radius={8} shadow="md" withBorder style={{ paddingBottom: '42px' }}>
+                        <Card.Section>
+                            <img src="/pic1.jpg" alt="" style={{ width: '100%', aspectRatio: '1 / 1', objectFit: 'cover' }} />
+                        </Card.Section>
+                        <Card.Section p={8}>
+                            <Text>
+                                Shoes!
+                            </Text>
+                        </Card.Section>
+                        <Card.Section p={8}>
+                            <Button fullWidth>
+                                Explore
+                            </Button>
+                        </Card.Section>
+                    </Card>
+                    </Carousel.Slide>
+
+                    <Carousel.Slide>
+                    <Card radius={8} shadow="md" withBorder style={{ paddingBottom: '42px' }}>
+                        <Card.Section>
+                            <img src="/pic2.jpg" alt="" style={{ width: '100%', aspectRatio: '1 / 1', objectFit: 'cover' }} />
+                        </Card.Section>
+                        <Card.Section p={8}>
+                            <Text>
+                                Shoes!
+                            </Text>
+                        </Card.Section>
+                        <Card.Section p={8}>
+                            <Button fullWidth>
+                                Explore
+                            </Button>
+                        </Card.Section>
+                    </Card>
+                    </Carousel.Slide>
+
+                    <Carousel.Slide>
+                    <Card radius={8} shadow="md" withBorder style={{ paddingBottom: '42px' }}>
+                        <Card.Section>
+                            <img src="/pic3.jpg" alt="" style={{ width: '100%', aspectRatio: '1 / 1', objectFit: 'cover' }} />
+                        </Card.Section>
+                        <Card.Section p={8}>
+                            <Text>
+                                Shoes!
+                            </Text>
+                        </Card.Section>
+                        <Card.Section p={8}>
+                            <Button fullWidth>
+                                Explore
+                            </Button>
+                        </Card.Section>
+                    </Card>
+                    </Carousel.Slide>
+
+                </Carousel>
+
+            </Stack>
+
         </main>
     )
 }
