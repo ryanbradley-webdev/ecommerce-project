9bf38d56e2b6bc6c68a8bcf2c64d829c797349b3
Ryan Bradley <ryanbradley315@gmail.com>
2023-07-03 17:37:02 -0500

    Refactor new item carousel generation to utilize useState and map over values.

--- src/routes/home/Home.tsx
+++ src/routes/home/Home.tsx
@@ -1,8 +1,32 @@
 import { Carousel } from "@mantine/carousel";
-import { Button, Card, Stack, MediaQuery, Text } from "@mantine/core";
+import { Stack, MediaQuery } from "@mantine/core";
 import NewItem from "./NewItem";
+import { useState } from "react";
+
+const PLACEHOLDER_ITEMS = [
+    {
+        name: 'New Balance Daddios',
+        price: '20.99',
+        image: '/pic1.jpg',
+        id: '1'
+    },
+    {
+        name: 'Nike Sportsball',
+        price: '69.99',
+        image: '/pic2.jpg',
+        id: '2'
+    },
+    {
+        name: 'Addidas Puma-Jordans',
+        price: '4.20',
+        image: '/pic3.jpg',
+        id: '3'
+    }
+]
 
 export default function Home() {
+    const [newItems, setNewItems] = useState(PLACEHOLDER_ITEMS)
+
     return (
         <main>
 
@@ -23,70 +47,35 @@ export default function Home() {
 
                 <div>
 
-                <h4
-                    style={{
-                        marginLeft: '11%'
-                    }}
-                >
-                    Recent Additions
-                </h4>
+                    <h4
+                        style={{
+                            marginLeft: '11%'
+                        }}
+                    >
+                        Recent Additions
+                    </h4>
 
-                <Carousel
-                    loop
-                    align='center'
-                    slideGap='xl'
-                    slideSize='80%'
-                    withIndicators
-                    styles={{
-                        indicator: {
-                            background: 'var(--color-indicator)'
-                        }
-                    }}
-                >
+                    <Carousel
+                        loop
+                        align='center'
+                        slideGap='xl'
+                        slideSize='80%'
+                        withIndicators
+                        styles={{
+                            indicator: {
+                                background: 'var(--color-indicator)'
+                            }
+                        }}
+                    >
 
-                    <NewItem
-                        image="/pic1.jpg"
-                        name='New Balance Daddios'
-                        price="20.99"
-                    />
+                        {newItems.map(item => (
+                            <NewItem
+                                {...item}
+                                key={item.id}
+                            />
+                        ))}
 
-                    <Carousel.Slide>
-                    <Card radius={8} shadow="md" withBorder pb={42}>
-                        <Card.Section>
-                            <img src="/pic2.jpg" alt="" style={{ width: '100%', aspectRatio: '1 / 1', objectFit: 'cover' }} />
-                        </Card.Section>
-                        <Card.Section p={8}>
-                            <Text>
-                                Shoes!
-                            </Text>
-                        </Card.Section>
-                        <Card.Section p={8}>
-                            <Button fullWidth>
-                                Explore
-                            </Button>
-                        </Card.Section>
-                    </Card>
-                    </Carousel.Slide>
-
-                    <Carousel.Slide>
-                    <Card radius={8} shadow="md" withBorder pb={42}>
-                        <Card.Section>
-                            <img src="/pic3.jpg" alt="" style={{ width: '100%', aspectRatio: '1 / 1', objectFit: 'cover' }} />
-                        </Card.Section>
-                        <Card.Section p={8}>
-                            <Text>
-                                Shoes!
-                            </Text>
-                        </Card.Section>
-                        <Card.Section p={8}>
-                            <Button fullWidth>
-                                Explore
-                            </Button>
-                        </Card.Section>
-                    </Card>
-                    </Carousel.Slide>
-
-                </Carousel>
+                    </Carousel>
 
                 </div>
 
