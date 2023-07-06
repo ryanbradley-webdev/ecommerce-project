d33ea31a582c29ad6636efeb457e71bf82ae27c0
Ryan Bradley <ryanbradley315@gmail.com>
2023-07-06 15:02:55 -0500

    Update button width to better fit in product cards.

--- components/ProductBtn.tsx
+++ components/ProductBtn.tsx
@@ -3,7 +3,12 @@ import { Link } from 'react-router-dom'
 
 export default function ProductBtn({ id }: { id: string }) {
     return (
-        <Link to={`/products/${id}`}>
+        <Link 
+            to={`/products/${id}`}
+            style={{
+                width: '100%'
+            }}    
+        >
             <Button
                 fullWidth
                 variant='outline'
