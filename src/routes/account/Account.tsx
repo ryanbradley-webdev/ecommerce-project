21aedf607e8851cff4b512d964c6a99b65c94862
Ryan Bradley <ryanbradley315@gmail.com>
2023-09-17 12:34:11 -0500

    Add basic content to accounts route.

--- src/routes/account/Account.tsx
+++ src/routes/account/Account.tsx
@@ -1,12 +1,13 @@
 import { useContext } from 'react'
 import styles from './account.module.css'
 import { AuthContext } from '../../contexts/AuthContext'
-import { Text } from '@mantine/core'
+import { Container, Flex, Stack, Text } from '@mantine/core'
 import { Link } from 'react-router-dom'
 import { IconAlertCircle } from '@tabler/icons-react'
 
 export default function Account() {
     const {
+        user,
         userData
     } = useContext(AuthContext)
 
@@ -17,9 +18,128 @@ export default function Account() {
 
             {
                 userData ? (
-                    <h1>
-                        {userData.user_id}
-                    </h1>
+                    <Stack>
+                        
+                        <Container
+                            w='100%'
+                        >
+
+                            <Text
+                                size={20}
+                                weight={600}
+                            >
+                                User: {user.email}
+                            </Text>
+
+                        </Container>
+
+                        <Stack>
+
+                            <Flex
+                                justify='space-between'
+                            >
+
+                                <Text>
+                                    Shipping Address
+                                </Text>
+
+                                <button>
+                                    Edit
+                                </button>
+
+                            </Flex>
+
+                            <Container>
+
+                                {
+                                    userData.shippingAddress ? (
+                                        <>
+                                            <Text>
+                                                {userData.shippingAddress.firstName}
+                                            </Text>
+
+                                            <Text>
+                                                {userData.shippingAddress.lastName}
+                                            </Text>
+                                        </>
+                                    ) : (
+                                        <Text>
+                                            No shipping address saved
+                                        </Text>
+                                    )
+                                }
+
+                            </Container>
+
+                        </Stack>
+
+                        <Stack>
+
+                            <Flex
+                                justify='space-between'
+                            >
+
+                                <Text>
+                                    Billing Address
+                                </Text>
+
+                                <button>
+                                    Edit
+                                </button>
+
+                            </Flex>
+
+                            <Container>
+
+                                {
+                                    userData.billingAddress ? (
+                                        <>
+                                            <Text>
+                                                {userData.billingAddress.firstName}
+                                            </Text>
+
+                                            <Text>
+                                                {userData.billingAddress.lastName}
+                                            </Text>
+                                        </>
+                                    ) : (
+                                        <Text>
+                                            No billing address saved
+                                        </Text>
+                                    )
+                                }
+
+                            </Container>
+
+                        </Stack>
+
+                        <Stack>
+
+                            <Text>
+                                Orders
+                            </Text>
+
+                            <Container>
+
+                                {
+                                    userData.orders.length > 0 ? (
+                                        userData.orders.map(order => (
+                                            <Flex>
+                                                {order}
+                                            </Flex>
+                                        ))
+                                    ) : (
+                                        <Text>
+                                            No past orders!
+                                        </Text>
+                                    )
+                                }
+
+                            </Container>
+
+                        </Stack>
+
+                    </Stack>
                 ) : (
                     <section
                         className={styles.no_user}
